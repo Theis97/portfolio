abc139b83932895cdf8c5c4053edb875cefca65a
Susan Theis <sttheis@hotmail.com>
2021-01-07 18:14:18 -0600

    wip reimplementing desktop project card animation

--- src/features/project_card/ProjectCard.js
+++ src/features/project_card/ProjectCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Spring, animated } from 'react-spring/renderprops';
+import { Spring, Transition, animated, config } from 'react-spring/renderprops';
 import styles from './ProjectCard.module.css';
 
 export class ProjectCard extends React.Component {
@@ -16,18 +16,41 @@ export class ProjectCard extends React.Component {
     const title = this.props.content.title;
     const desc = React.cloneElement(this.props.content.desc, {className: textStyle});
 
+    let header = <h3>{title}</h3>;
+    let body = desc;
+    if(window.innerWidth > 900) {
+      header = <Spring
+          native
+          config={{ precision: 1 }}
+          from={{ transform: this.props.isActive ? 'translateY(275px)' : 'translateY(0px)' }}
+          to={{ transform: this.props.isActive ?  'translateY(0px)' : 'translateY(275px)' }}>
+          {props => (<animated.h3 style={props}>{title}</animated.h3>)}
+        </Spring>;
+
+      body = <Spring
+        native
+        from={{ opacity: 0 }}
+        to={{ opacity: 1 }}>
+        {props => (<animated.div style={props}>{desc}</animated.div>)}
+      </Spring>;
+
+    } else {
+      body = <Spring
+        native
+        from={{ height: this.props.isActive ? 0 : 'auto' }}
+        to={{ height: this.props.isActive ? 'auto' : 0 }}>
+        {props => (<animated.div style={props}>{desc}</animated.div>)}
+      </Spring>;
+    }
+
     return (
-      <button onClick={this.props.cardClicked} style={{backgroundImage: 'url(' + imageURL + ')'}} className={currCardStyle}>
-        <h3>{title}</h3>
-        <Spring native
-          from={{ height: this.props.isActive ? 0 : 'auto' }}
-          to={{ height: this.props.isActive ? 'auto' : 0 }}>
-          {props => (
-            <animated.div style={props}>
-              {desc}
-            </animated.div>
-          )}
-        </Spring>
+      <button
+        onClick={this.props.cardClicked}
+        style={{backgroundImage: 'url(' + imageURL + ')'}}
+        className={currCardStyle}
+      >
+        {header}
+        {body}
       </button>
     );
   }
