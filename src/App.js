f92fbffc54999a8917b5385b6f58fb02f924733b
Susan Theis <sttheis97@gmail.com>
2023-02-27 22:13:50 -0600

    Update information

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import myFace from './assets/me.png';
-import { ProjectDisplay } from './features/project_display/ProjectDisplay.js';
-import './App.css';
-import nav from './nav.module.css';
-import title from './title.module.css';
-import overview from './overview.module.css';
-import projects from './projects.module.css';
-import contact from './contact.module.css';
+import React from "react";
+import myFace from "./assets/me.png";
+import { ProjectDisplay } from "./features/project_display/ProjectDisplay.js";
+import "./App.css";
+import nav from "./nav.module.css";
+import title from "./title.module.css";
+import overview from "./overview.module.css";
+import projects from "./projects.module.css";
+import contact from "./contact.module.css";
 
 function App() {
   const birthday = new Date(868021200000); // July 4th, 1997 8am CST in UTC
   const today = new Date();
-  const age = Math.floor((today - birthday)/31556926000); // convert from milliseconds to years
+  const age = Math.floor((today - birthday) / 31556926000); // convert from milliseconds to years
 
   return (
     <div className="App">
       <nav className={nav.nav}>
-        <h2><a href="#overview">About Me</a></h2>
-        <h2><a href="#projects">Projects</a></h2>
-        <h2><a href="#contact">Contact</a></h2>
+        <h2>
+          <a href="#overview">About Me</a>
+        </h2>
+        <h2>
+          <a href="#projects">Projects</a>
+        </h2>
+        <h2>
+          <a href="#contact">Contact</a>
+        </h2>
       </nav>
       <section className={title.title}>
         <div className={title.title_text}>
@@ -33,32 +39,37 @@ function App() {
         </div>
         <div className={overview.about}>
           <p>
-            I'm a {age} year old Software Developer living in the Chicago suburbs.
-            I got my Bachelor's degree in Computer Science from the University
-            of Illinois at Urbana-Champaign, where I was also a member of the
-            Marching Illini (Go Illini!). In addition to programming, I also enjoy
-            drawing, writing, playing video games, and obsessing over my favorite
-            shows with my friends.
+            I'm a {age} year old Software Developer living in Illinois. I got my
+            Bachelor's degree in Computer Science from the University of
+            Illinois at Urbana-Champaign, where I was also a member of the
+            Marching Illini (Go Illini!). In addition to programming, I also
+            enjoy drawing, writing, playing video games, and obsessing over my
+            favorite shows with my friends.
           </p>
           <p>
-            My mission is to create software that makes the world a better place,
-            whether that's by providing solutions for tough problems or by simply
-            bringing people joy. Lately, I've been working with a small team to
-            create a virtual reality experience that aims to promote the user's
-            mental health and let them connect with other people. You can read
-            more about it in the projects section below.
+            My mission is to create software that makes the world a better
+            place, whether that's by providing solutions for tough problems or
+            by simply bringing people joy. For the past 2 years, I've been
+            working as a UI developer with{" "}
+            <a href="https://www.wolflair.com/">Lone Wolf Development</a>.
           </p>
         </div>
       </section>
       <section id="projects" className={projects.projects}>
         <h2>My Projects</h2>
-        <ProjectDisplay/>
+        <ProjectDisplay />
       </section>
       <section id="contact" className={contact.contact}>
         <h2>Want to get in touch?</h2>
         <ul>
-          <li>✉ <a href="mailto:sttheis97@gmail.com">sttheis97@gmail.com</a></li>
-          <li><a href="https://www.linkedin.com/in/susan-theis-260465187/">Linkedin</a></li>
+          <li>
+            ✉ <a href="mailto:sttheis97@gmail.com">sttheis97@gmail.com</a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/susan-theis-260465187/">
+              Linkedin
+            </a>
+          </li>
         </ul>
       </section>
     </div>
--- src/features/project_display/ProjectDisplay.js
+++ src/features/project_display/ProjectDisplay.js
@@ -1,53 +1,105 @@
-import React from 'react';
-import styles from './ProjectDisplay.module.css';
-import { ProjectCard } from '../project_card/ProjectCard.js';
-import mfImg from '../../assets/mf-wallpaper.png'
-import doodleImg from '../../assets/doodle3.png'
-import cloudImg from '../../assets/clouds.png'
-import starsImg from '../../assets/stars.png'
+import React from "react";
+import styles from "./ProjectDisplay.module.css";
+import { ProjectCard } from "../project_card/ProjectCard.js";
+import mfImg from "../../assets/mf-wallpaper.png";
+import doodleImg from "../../assets/doodle3.png";
+import cloudImg from "../../assets/clouds.png";
+import starsImg from "../../assets/stars.png";
 
 const cards = [
   {
     title: "Project Magic Forest",
-    desc: <ul>
-            <li>Magic Forest is a multiplayer virtual reality game with a soothing, mystical atmosphere where players can reflect and store their thoughts while connecting with their friends.</li>
-            <li>The project is a collaboration between <a href="http://dang-brandon.com/">Brandon Dang</a>, <a href="http://dawnuxi.com/">Dawn Nguyen</a>, and myself.</li>
-            <li>Our goal is to create a virtual space that promotes the user's mental health and helps them make meaningful connections with other people.</li>
-            <li>We’re currently developing the project with Unity and Mirror Networking.</li>
-            <li>Most of my work on the project consists of implementing game mechanics and multiplayer functionality.</li>
-          </ul>,
-    imageURL: mfImg
+    desc: (
+      <ul>
+        <li>
+          Magic Forest was a multiplayer virtual reality game with a soothing,
+          mystical atmosphere where players could reflect and store their
+          thoughts while connecting with their friends.
+        </li>
+        <li>
+          The project was a collaboration between{" "}
+          <a href="http://dang-brandon.com/">Brandon Dang</a>,{" "}
+          <a href="http://dawnuxi.com/">Dawn Nguyen</a>, and myself.
+        </li>
+        <li>
+          Our goal was to create a virtual space that promoted users' mental
+          health and helped them make meaningful connections with other people.
+        </li>
+        <li>The project was developed with Unity and Mirror Networking.</li>
+        <li>
+          Most of my work on the project consisted of implementing game
+          mechanics and multiplayer functionality.
+        </li>
+        <li>We are no longer actively working on this project.</li>
+      </ul>
+    ),
+    imageURL: mfImg,
   },
   {
     title: "WebDoodler",
-    desc: <ul>
-            <li>WebDoodler is a little drawing app I built from scratch with pure JavaScript, HTML, and CSS.</li>
-            <li>I originally created it for my programming studio final while in college. It was a solo project, and we had a total of 4 weeks to work on it.</li>
-            <li>It features a basic brush, an eraser, a line tool, an eyedropper tool, a palette to save colors, variable brush sizes, a selection tool, a layer system, an undo button, a few basic filters, and a way to save images you made or upload your own image to work with.</li>
-            <li>You can find the source code for it <a href="https://github.com/Theis97/Webdoodler">here</a>.</li>
-          </ul>,
-    imageURL: doodleImg
+    desc: (
+      <ul>
+        <li>
+          WebDoodler is a little drawing app I built from scratch with pure
+          JavaScript, HTML, and CSS.
+        </li>
+        <li>
+          I originally created it for my programming studio final while in
+          college. It was a solo project, and we had a total of 4 weeks to work
+          on it.
+        </li>
+        <li>
+          It features a basic brush, an eraser, a line tool, an eyedropper tool,
+          a palette to save colors, variable brush sizes, a selection tool, a
+          layer system, an undo button, a few basic filters, and a way to save
+          images you made or upload your own image to work with.
+        </li>
+        <li>
+          You can find the source code for it{" "}
+          <a href="https://github.com/Theis97/Webdoodler">here</a>.
+        </li>
+      </ul>
+    ),
+    imageURL: doodleImg,
   },
   {
     title: "This Website",
-    desc: <ul>
-            <li>I built this site with React. Most of the animations for these panels use <a href="https://www.react-spring.io/">react-spring</a>.</li>
-            <li>I'm still in the process of polishing the site up.</li>
-            <li>You can check out the source code for it <a href="https://github.com/Theis97/portfolio">here</a> if you'd like.</li>
-          </ul>,
-    imageURL: cloudImg
+    desc: (
+      <ul>
+        <li>
+          I built this site with React. Most of the animations for these panels
+          use <a href="https://www.react-spring.io/">react-spring</a>.
+        </li>
+        <li>
+          I still consider this site to be a work in progress, although I've had
+          little time to work on it lately.
+        </li>
+        <li>
+          You can check out the source code for it{" "}
+          <a href="https://github.com/Theis97/portfolio">here</a> if you'd like.
+        </li>
+      </ul>
+    ),
+    imageURL: cloudImg,
   },
   {
     title: "More to come, in the future...",
-    desc: <ul>
-            <li>Expect to see more projects listed here in the future! In the meantime, why not check out <a href="https://github.com/Theis97">my Github?</a> It has some of the code from the projects listed here, as well as smaller things that didn't make the cut for this showcase.</li>
-          </ul>,
-    imageURL: starsImg
-  }
+    desc: (
+      <ul>
+        <li>
+          Expect to see more projects listed here in the future! In the
+          meantime, why not check out{" "}
+          <a href="https://github.com/Theis97">my Github?</a> It has some of the
+          code from the projects listed here, as well as smaller things that
+          didn't make the cut for this showcase.
+        </li>
+      </ul>
+    ),
+    imageURL: starsImg,
+  },
 ];
 
 export class ProjectDisplay extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = { isCardActive: false, activeId: 0 };
@@ -57,7 +109,7 @@ export class ProjectDisplay extends React.Component {
     if (e.target.tagName === "BUTTON" || e.target.tagName === "DIV") {
       this.setState((state) => ({
         isCardActive: state.activeId !== id ? true : !state.isCardActive,
-        activeId: id
+        activeId: id,
       }));
     }
   }
@@ -70,9 +122,9 @@ export class ProjectDisplay extends React.Component {
             key={id}
             content={card}
             cardClicked={this.cardClicked.bind(this, id)}
-            isActive={this.state.isCardActive && (this.state.activeId === id)}
-          />)
-        )}
+            isActive={this.state.isCardActive && this.state.activeId === id}
+          />
+        ))}
       </div>
     );
   }
